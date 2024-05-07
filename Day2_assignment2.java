62566f96a06c711e5c8eac4f253eea580c9b9a2a
Thanushya Loganathan <115723536+thanushyal@users.noreply.github.com>
2024-05-08 00:01:30 +0530

    Create Day2_assignment2.java

---
+++ Day2_assignment2.java
@@ -0,0 +1,26 @@
+abstract class route_bus
+{
+  abstract public void displayplot ();
+}
+class bustand extends route_bus
+{
+  public void displayplot ()
+  {
+	   int plot_no = 15;
+	   String plot_name = "tirupur";
+     System.out.println ("plot for root_bus in bustand :" + plot_no);
+     System.out.println ("plot-name for routebus:" + plot_name);
+  }
+
+}
+
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+	  route_bus r = new bustand ();
+	  r.displayplot ();
+
+  }
+}
