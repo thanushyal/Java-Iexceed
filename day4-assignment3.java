26ff89c204e2492390f4c1f1054fa39392f9fd1b
Thanushya Loganathan <115723536+thanushyal@users.noreply.github.com>
2024-05-09 18:35:40 +0530

    Create day4-assignment3.java

---
+++ day4-assignment3.java
@@ -0,0 +1,42 @@
+//two dimensional array
+import java.io.DataInputStream;
+public class dblearr 
+{
+   public static void main(String args[])
+   {
+       DataInputStream d1=new DataInputStream(System.in);
+       int mat1[][]=new int[4][3];
+       for(int i=0;i<4;i++)
+       {
+           for(int j=0;j<3;j++)
+           {
+               try
+               { 
+                   mat1[i][j]=Integer.valueOf(d1.readLine()).intValue();//getting input to store in array
+               }
+               catch(Exception d)
+               {
+
+               }
+
+           }
+       }
+       for(int i=0;i<4;i++)
+           {
+               for(int j=0;j<3;j++)
+               {
+                   System.out.print(mat1[i][j]);//displaying array elements
+               System.out.println(" ");
+           }
+       
+       
+
+
+   }
+   
+}
+
+
+
+
+
