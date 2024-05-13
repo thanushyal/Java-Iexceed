aa3b84f7423b92b47976771b1fec8ecd21a03863
Thanushya Loganathan <115723536+thanushyal@users.noreply.github.com>
2024-05-13 23:15:04 +0530

    Create day06-assignment5.java

---
+++ day06-assignment5.java
@@ -0,0 +1,31 @@
+//substring
+public class pro7 {
+
+   public static void main(String args[])
+
+   {
+
+       String str1="welcome to java programming";
+
+       System.out.println(str1.substring(11,16));
+
+   }
+
+}
+
+
+//replace
+
+public class pro9 {
+
+   public static void main(String args[])
+
+   {
+
+       String s="hello welcome to iexceed";
+
+       System.out.println(s.replace("iexceed","TCS"));
+
+}
+
+}
